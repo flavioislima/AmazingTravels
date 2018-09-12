2f6a6a3865f32f47c62e51bb36cf7bac43e5eef4
Flavio Lima <flavioislima@gmail.com>
2018-09-12 00:00:26 -0300

    Created reducer and main actions

---
+++ src/store/reducers/root.js
@@ -0,0 +1,17 @@
+const initialState = {
+    places: [],
+    selectedPlaces: null
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case value: 
+
+            break;
+    
+        default:
+            return state;
+    }
+}
+
+export default reducer
